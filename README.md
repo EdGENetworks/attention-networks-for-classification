39ff9d67cde2b97a617a131ff7690e17d8372283
Sandeep Tammu <sandeep.t@edgenetworks.in>
2017-03-02 15:49:39 +0530

    cleaned model, added bias, updated readme

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 
 ##  Hierarchical Attention Networks for Document Classification
 
-We know that documents have a hierarchical structure, words combine to form sentences and sentences combine to form documents. [This paper](https://www.cs.cmu.edu/~diyiy/docs/naacl16.pdf) exploits that structure to build a classification model. This is an implementation of the model in PyTorch.
+We know that documents have a hierarchical structure, words combine to form sentences and sentences combine to form documents. We can try to learn that structure or we can input this hierarchical structure into the model and see if it improves the performance of existing models. [This paper](https://www.cs.cmu.edu/~diyiy/docs/naacl16.pdf) exploits that structure to build a classification model. 
 
-*Note:*  I did not include a bias term in doing the initial linear map of the attention network. If I included that term, I was not able to do backpropagation. I am hoping to fix it, suggestions are welcome.
+This is an (close) implementation of the model in PyTorch. 
+
+### Note:
+
+1. I jointly optimize both the word and sentence attention models with the same optimizer.
+2. The minibatches are padded with zeros. This can be improved, one can sort senteces with similar length together, and minimize the paddings.
+3. Pytorch does not yet support gradient masking, so padded zeros will have gradients flowing through them during backpropagation. One can create a mask, but since I am interested in using Bidirectional GRU, it is not possible to use a mask. I've seen that variable length RNN supoort is coming soon to Pytorch as well.
 
 This picture from [Explosion blog](https://explosion.ai/blog/deep-learning-formula-nlp) explains the structure perfectly.
 
@@ -11,7 +17,8 @@ This picture from [Explosion blog](https://explosion.ai/blog/deep-learning-formu
 
 ### Notebook
 
-The notebook contains an example of trained model on IMDB movie review dataset. I did not do any hyperparameter tuning or validation checks. Here is the training loss plot on the same dataset.
+The notebook contains an example of trained model on IMDB movie review dataset. I could not get the original IMDB dataset that the paper referred to, so I have used [this data](http://ir.hit.edu.cn/~dytang/paper/acl2015/dataset.7z)
+
+The best accuracy that I got was around ~ 0.35. This dataset has only 84919 samples and 10 classes. Here is the training loss for the dataset. 
 
 ![alt text](imdb_data_attn.png "Document Classification")
-
--- model.py
+++ model.py
@@ -1,7 +1,31 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
+
+
+# ## Functions to accomplish attention
+
+def batch_matmul_bias(seq, weight, bias, nonlinearity=''):
+    s = None
+    bias_dim = bias.size()
+    for i in range(seq.size(0)):
+        _s = torch.mm(seq[i], weight) 
+        _s_bias = _s + bias.expand(bias_dim[0], _s.size()[0]).transpose(0,1)
+        if(nonlinearity=='tanh'):
+            _s_bias = torch.tanh(_s_bias)
+        _s_bias = _s_bias.unsqueeze(0)
+        if(s is None):
+            s = _s_bias
+        else:
+            s = torch.cat((s,_s_bias),0)
+    return s.squeeze()
+
+
 
 def batch_matmul(seq, weight, nonlinearity=''):
     s = None
@@ -15,6 +39,8 @@ def batch_matmul(seq, weight, nonlinearity=''):
         else:
             s = torch.cat((s,_s),0)
     return s.squeeze()
+
+
 
 def attention_mul(rnn_outputs, att_weights):
     attn_vectors = None
@@ -30,9 +56,12 @@ def attention_mul(rnn_outputs, att_weights):
     return torch.sum(attn_vectors, 0)
 
 
-class AttentionWordRNN(nn.Module):    
+# ## Word attention model with bias
+
+class AttentionWordRNN(nn.Module):
     
-    def __init__(self, batch_size, num_tokens, embed_size, word_gru_hidden, bidirectional= True):     
+    
+    def __init__(self, batch_size, num_tokens, embed_size, word_gru_hidden, bidirectional= True):        
         
         super(AttentionWordRNN, self).__init__()
         
@@ -46,40 +75,44 @@ class AttentionWordRNN(nn.Module):
         if bidirectional == True:
             self.word_gru = nn.GRU(embed_size, word_gru_hidden, bidirectional= True)
             self.weight_W_word = nn.Parameter(torch.Tensor(2* word_gru_hidden,2*word_gru_hidden))
-#             self.bias_word = nn.Parameter(torch.Tensor(2* word_gru_hidden,1))
+            self.bias_word = nn.Parameter(torch.Tensor(2* word_gru_hidden,1))
             self.weight_proj_word = nn.Parameter(torch.Tensor(2*word_gru_hidden, 1))
         else:
             self.word_gru = nn.GRU(embed_size, word_gru_hidden, bidirectional= False)
             self.weight_W_word = nn.Parameter(torch.Tensor(word_gru_hidden, word_gru_hidden))
-#             self.bias_word = nn.Parameter(torch.Tensor(word_gru_hidden,1))
-            self.weight_proj_word = nn.Parameter(torch.Tensor(word_gru_hidden, 1))            
+            self.bias_word = nn.Parameter(torch.Tensor(word_gru_hidden,1))
+            self.weight_proj_word = nn.Parameter(torch.Tensor(word_gru_hidden, 1))
+            
         self.softmax_word = nn.Softmax()
         self.weight_W_word.data.uniform_(-0.1, 0.1)
-        self.weight_proj_word.data.uniform_(-0.1,0.1)        
+        self.weight_proj_word.data.uniform_(-0.1,0.1)
+
+        
         
     def forward(self, embed, state_word):
+        # embeddings
         embedded = self.lookup(embed)
-#         print embedded.size()
         # word level gru
         output_word, state_word = self.word_gru(embedded, state_word)
 #         print output_word.size()
-        # word level bat
-        word_squish = batch_matmul(output_word, self.weight_W_word, nonlinearity='tanh')
-#         print word_squish.size()
+        word_squish = batch_matmul_bias(output_word, self.weight_W_word,self.bias_word, nonlinearity='tanh')
         word_attn = batch_matmul(word_squish, self.weight_proj_word)
         word_attn = self.softmax_word(word_attn)
-        word_attn_vectors = attention_mul(output_word, word_attn)
-#         print word_attn_vectors.size()        
-        return word_attn_vectors, state_word
+        word_attn_vectors = attention_mul(output_word, word_attn)        
+        return word_attn_vectors, state_word, word_attn
     
     def init_hidden(self):
         if self.bidirectional == True:
             return Variable(torch.zeros(2, self.batch_size, self.word_gru_hidden))
         else:
             return Variable(torch.zeros(1, self.batch_size, self.word_gru_hidden))        
-        
-        
-class AttentionSentRNN(nn.Module):    
+
+
+# ## Sentence Attention model with bias
+
+
+class AttentionSentRNN(nn.Module):
+    
     
     def __init__(self, batch_size, sent_gru_hidden, word_gru_hidden, n_classes, bidirectional= True):        
         
@@ -95,13 +128,13 @@ class AttentionSentRNN(nn.Module):
         if bidirectional == True:
             self.sent_gru = nn.GRU(2 * word_gru_hidden, sent_gru_hidden, bidirectional= True)        
             self.weight_W_sent = nn.Parameter(torch.Tensor(2* sent_gru_hidden ,2* sent_gru_hidden))
-#             self.bias_sent = nn.Parameter(torch.Tensor(2* sent_gru_hidden,1))
+            self.bias_sent = nn.Parameter(torch.Tensor(2* sent_gru_hidden,1))
             self.weight_proj_sent = nn.Parameter(torch.Tensor(2* sent_gru_hidden, 1))
             self.final_linear = nn.Linear(2* sent_gru_hidden, n_classes)
         else:
             self.sent_gru = nn.GRU(word_gru_hidden, sent_gru_hidden, bidirectional= True)        
             self.weight_W_sent = nn.Parameter(torch.Tensor(sent_gru_hidden ,sent_gru_hidden))
-#             self.bias_sent = nn.Parameter(torch.Tensor(sent_gru_hidden,1))
+            self.bias_sent = nn.Parameter(torch.Tensor(sent_gru_hidden,1))
             self.weight_proj_sent = nn.Parameter(torch.Tensor(sent_gru_hidden, 1))
             self.final_linear = nn.Linear(sent_gru_hidden, n_classes)
         self.softmax_sent = nn.Softmax()
@@ -109,28 +142,19 @@ class AttentionSentRNN(nn.Module):
         self.weight_W_sent.data.uniform_(-0.1, 0.1)
         self.weight_proj_sent.data.uniform_(-0.1,0.1)
         
+        
     def forward(self, word_attention_vectors, state_sent):
-        '''
-        Pass a combination of word level attention vectors as input
-        '''
-#         print word_attention_vectors.size()
-        # sent level gru
-        output_sent, state_sent = self.sent_gru(word_attention_vectors, state_sent)
-#         print state_sent.size()        
-        # sent level attention
-        sent_squish = batch_matmul(output_sent, self.weight_W_sent ,nonlinearity='tanh')
+        output_sent, state_sent = self.sent_gru(word_attention_vectors, state_sent)        
+        sent_squish = batch_matmul_bias(output_sent, self.weight_W_sent,self.bias_sent, nonlinearity='tanh')
         sent_attn = batch_matmul(sent_squish, self.weight_proj_sent)
-#         print sent_attn.size()
         sent_attn = self.softmax_sent(sent_attn)
         sent_attn_vectors = attention_mul(output_sent, sent_attn)        
         # final classifier
-#         print sent_attn_vectors.squeeze(0).size()
         final_map = self.final_linear(sent_attn_vectors.squeeze(0))
-#         final_cls = self.final_softmax(final_map)
-        return F.log_softmax(final_map), state_sent
+        return F.log_softmax(final_map), state_sent, sent_attn
     
     def init_hidden(self):
         if self.bidirectional == True:
             return Variable(torch.zeros(2, self.batch_size, self.sent_gru_hidden))
         else:
-            return Variable(torch.zeros(1, self.batch_size, self.sent_gru_hidden))   
+            return Variable(torch.zeros(1, self.batch_size, self.sent_gru_hidden))
